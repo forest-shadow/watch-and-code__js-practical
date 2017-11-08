96a569c8db8c0fb5db74bdc0da6c73a1c1b95f78
forest-shadow <mamasay@yandex.ru>
2017-11-08 15:38:13 +0300

    app todoList v9: get out of console; get rid of unnecessary code

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 var todoList = {
     todos: [],
-    displayTodos: function() {
-        if(this.todos.length ===0) {
-            console.log('Your todo list is empty!');
-        } else {
-            console.log('My todos:');
-            
-            for(var i = 0; i < this.todos.length; i++) {
-                if(this.todos[i].completed === true) {
-                    console.log('(x)', this.todos[i].todoText);
-                } else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function(todoText) {
         this.todos.push({
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
     },
     changeTodo: function(position, todoText) {
         this.todos[position].todoText = todoText;
-        this.displayTodos();
     },
     toggleCompleted: function(position) {
         var todo = this.todos[position];
 
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     deleteTodo: function (position) {
         this.todos.splice(position, 1);
-        this.displayTodos();
     },
     toggleAll: function () {
         var totalTodos = this.todos.length,
@@ -58,20 +39,16 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
-
-        this.displayTodos();
     }
 };
 
 var handlers = {
-    displayTodos: function() {
-        todoList.displayTodos();
-    },
     addTodo: function() {
         var addTodoTextInput = document.getElementById('addTodoTextInput');
 
         todoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = '';
+        view.displayTodos();
     },
     changeTodo: function() {
         var changeTodoTextInput = document.getElementById('changeTodoTextInput'),
@@ -80,20 +57,47 @@ var handlers = {
         todoList.changeTodo(changeTodoPositionInput.value, changeTodoTextInput.value );
         changeTodoTextInput.value = '';
         changeTodoPositionInput.value = '';
+        view.displayTodos();
     },
     deleteTodo: function () {
         var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
 
         todoList.deleteTodo(deleteTodoPositionInput.value);
         deleteTodoPositionInput.value = '';
+        view.displayTodos();
     },
     toggleCompleted: function () {
         var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
 
         todoList.toggleCompleted(toggleCompletedPositionInput.value);
         toggleCompletedPositionInput.value = '';
+        view.displayTodos();
     },
     toggleAll: function() {
         todoList.toggleAll();
+        view.displayTodos();
+    }
+};
+
+var view = {
+    displayTodos: function() {
+        var todoUl = document.querySelector('ul');
+
+        todoUl.innerHTML = '';
+
+        for( var i = 0; i < todoList.todos.length; i++) {
+            var todoLi = document.createElement('li'),
+                todo = todoList.todos[i],
+                todoTextWithCompletion = '';
+
+            if(todo.completed === true) {
+                todoTextWithCompletion = '(x) ' + todo.todoText;
+            } else {
+                todoTextWithCompletion = '( ) ' + todo.todoText;
+            }
+
+            todoLi.textContent = todoTextWithCompletion;
+            todoUl.appendChild(todoLi);
+        }
     }
 };
